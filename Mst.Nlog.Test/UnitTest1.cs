95ccd91625b8202b1e1b0e7215dbc61106b120db
Mehran Sattary <mehran999m9@gmail.com>
2024-10-06 21:11:43 +0330

    Add project files.

---
+++ Mst.Nlog.Test/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Moq;
+using Mst.Logging.CustomLogs;
+using Mst.Logging.Enums;
+using Xunit.Sdk;
+
+namespace Mst.Nlog.Test;
+
+public class UnitTest1
+{
+  [Fact]  
+    public void LogByFavoriteLibrary_ShouldLogCorrectly_ForDifferentLogLevels()  
+    {  
+        // Arrange  
+        var mockHttpContextAccessor = new Mock<IHttpContextAccessor>();  
+        var logger = NLog.LogManager.GetLogger("TestLogger");  
+        var logAdapter = new NLogAdapter<TestClass>(mockHttpContextAccessor.Object);  
+        var log = new Log { Level = LogLevelEnum.Information }; // مثال با یک سطح لاگ   
+
+        // حفظ حالت نمایش از NLog  
+        var mockLogger = new Mock<ILogger>();  
+
+        using (LogManager.SuppressInternalTrace())  
+        {  
+            LogManager.UseLocalization(false);  
+            LogManager.Configuration = new NLog.Config.LoggingConfiguration();  
+            LogManager.Configuration.AddRule(LogLevel.Debug, LogLevel.Fatal, mockLogger.Object);  
+            LogManager.ReconfigExistingLoggers();  
+            
+            // Act  
+            logAdapter.LogByFavoriteLibrary(log, null);  
+
+            // Assert  
+            mockLogger.Verify(x => x.Info(It.IsAny<Exception>(), It.IsAny<string>()), Times.Once); // برای سطح Information  
+        }  
+    }  
+}
---
+++ Mst.Nlog/NLogAdapter.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Mst.Logging.CustomLogs;
+using Mst.Logging.Enums;
+using Mst.Logging.Logger;
+
+namespace Mst.Nlog;
+
+public class NLogAdapter<T> : Logger<T>
+{
+    public NLogAdapter(IHttpContextAccessor httpContextAccessor)
+        :base(httpContextAccessor)
+    {
+            
+    }
+
+ 
+
+    protected override void LogByFavoriteLibrary(Log log, Exception exception)
+    {
+        string loggerMessage = log.ToString();
+        var logger = NLog.LogManager.GetLogger(name: typeof(T).ToString());
+
+        switch (log.Level)
+        {
+            case LogLevelEnum.Trace:
+                {
+                    logger.Trace(exception, message: loggerMessage);
+                    break;
+                }
+
+            case LogLevelEnum.Debug:
+                {
+                    logger.Debug(exception, message: loggerMessage);
+                    break;
+                }
+
+            case LogLevelEnum.Information:
+                {
+                    logger.Info(exception, message: loggerMessage);
+                    break;
+                }
+
+            case LogLevelEnum.Warning:
+                {
+                    logger.Warn(exception, message: loggerMessage);
+                    break;
+                }
+
+            case LogLevelEnum.Error:
+                {
+                    logger.Error(exception, message: loggerMessage);
+                    break;
+                }
+
+            case LogLevelEnum.Critical:
+                {
+                    logger.Fatal(exception, message: loggerMessage);
+                    break;
+                }
+        }
+    }
+}
